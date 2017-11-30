b15c74a59c2376193a5ad7db57c217de9f33e8f3
thebat93 <thebat93@sibmail.com>
2017-11-30 22:13:23 +0300

    Update to Angular 5

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Angular 4 test project (Angular - The Complete Course Project)
+Angular 5 test project (Angular - The Complete Course Project)
+Recipe book + Shopping list
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import "leaflet";
@@ -35,7 +35,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
